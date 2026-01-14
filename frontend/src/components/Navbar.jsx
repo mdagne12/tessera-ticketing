760e6d4db7b39b8ca83e0a09e15a1fbf05437a29
milkii <mdagne12@terpmail.umd.edu>
2026-01-14 10:01:04 -0500

    added linear gradient to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { BsPersonCircle } from "react-icons/bs";
 
 function Navbar() {
   return (
-    <Flex bgGradient="linear(to-r, teal.600, blue.600)" color="white" p="4" alignItems="center">
+    // teal.600
+    <Flex bgGradient="linear(to-r, blue.500, blue.600)" color="white" p="4" alignItems="center">
       <Box p="2">
         <Text fontFamily="Helvetica" fontSize="2xl" fontWeight="bold">Tessera Events</Text>
       </Box>
